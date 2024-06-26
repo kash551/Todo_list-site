e27115d6fbe315b1fb7da48891909900f3ef67bf
kalyani kommula <kalyanib.tech505@gmail.com>
2024-06-26 12:55:48 +0000

    added some changes

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from todo import views
 
 urlpatterns = [
-    path('', views.index, name="todo"),
+    path(' ', views.index, name="todo"),
     path('del/<str:item_id>', views.remove, name="del"),
     path('admin/', admin.site.urls),
 ]
