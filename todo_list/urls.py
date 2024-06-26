5cc172e17244763e6af0b53d640e9ab392d4cbf9
Kashif Hussain <kashifh2005@gmail.com>
2024-06-26 11:20:47 +0000

    edited the urls.py file in todo_list to import views and made 2 new paths

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo import views
 
 urlpatterns = [
+    path('', views.index, name="todo"),
+    path('del/<str:item_id>', views.remove, name="del"),
     path('admin/', admin.site.urls),
 ]
