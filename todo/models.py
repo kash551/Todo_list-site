163f7dbe8d5c384564665a4c96aac1a45779ea35
Charlie Lambino-Worthington <charlie.m.k.worthington@gmail.com>
2024-06-26 12:47:53 +0000

    add models for to_do and user_profile

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
+ 
+ STATUS = ((0, "Incomplete"), (1, "Complete"))
+ 
+class Todo(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    details = models.TextField()
+    user_id = user_profile.user_id
+    username = user_profile.username
+    spoons_required = IntegerField() 
+    date = models.DateTimeField(default=timezone.now)
+    status = models.IntegerField(choices=STATUS, default=0)
 
-# Create your models here.
+ 
+    def __str__(self):
+        return self.title
+
+class user_profile(models.Model):
+    user_id = IntegerField()
+    username = models.CharField(max_length=30, unique=True)
+    max_spoons = IntegerField()
