38abf91a5801e632115da487b285eb5fdfa21e47
kalyani kommula <kalyanib.tech505@gmail.com>
2024-06-27 12:38:21 +0000

    register user profile model in admin

--- todo/admin.py
+++ todo/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Todo 
+from .models import Todo, UserProfile
 # Register your models here.
 admin.site.register(Todo)
+admin.site.register(UserProfile)
